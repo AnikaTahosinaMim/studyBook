66156f5da722fc5756ff12ea07c102787ca83cad
anika557 <ummehabiba19758@gmail.com>
2026-05-22 22:02:44 +0600

    comment

--- src/components/HowItWorks .jsx
+++ src/components/HowItWorks .jsx
@@ -3,12 +3,9 @@ import { FaSearch, FaCreditCard, FaBookOpen } from 'react-icons/fa'
 const HowItWorks = () => {
 	return (
 		<div className='py-16 px-6 text-center bg-gray-50'>
-			{/* Title */}
 			<h2 className='text-3xl font-bold mb-12'>How It Works</h2>
 
-			{/* Cards */}
 			<div className='grid md:grid-cols-3 gap-6 max-w-5xl mx-auto'>
-				{/* Step 1 */}
 				<div className='p-6 border rounded-xl bg-white shadow hover:scale-105 transition duration-300'>
 					<FaSearch className='text-4xl text-purple-600 mx-auto mb-3 animate-bounce' />
 					<h3 className='text-xl font-semibold'>1. Browse Rooms</h3>
@@ -17,7 +14,6 @@ const HowItWorks = () => {
 					</p>
 				</div>
 
-				{/* Step 2 */}
 				<div className='p-6 border rounded-xl bg-white shadow hover:scale-105 transition duration-300'>
 					<FaCreditCard className='text-4xl text-purple-600 mx-auto mb-3 animate-pulse' />
 					<h3 className='text-xl font-semibold'>2. Book Instantly</h3>
@@ -26,7 +22,6 @@ const HowItWorks = () => {
 					</p>
 				</div>
 
-				{/* Step 3 */}
 				<div className='p-6 border rounded-xl bg-white shadow hover:scale-105 transition duration-300'>
 					<FaBookOpen className='text-4xl text-purple-600 mx-auto mb-3 animate-bounce' />
 					<h3 className='text-xl font-semibold'>3. Start Studying</h3>
